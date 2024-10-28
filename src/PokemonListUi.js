18d606a1f77f521fd50ff0a880b4dae87584c2ca
dardarcinmilena.becerra.contractor.cidentity <darcinmilena.becerra.contractor.cidentity@bbva.com>
2024-10-28 18:21:02 -0500

    refactor(project): The logic of the UI component was separated into a DM component

--- src/PokemonListUi.js
+++ src/PokemonListUi.js
@@ -5,31 +5,15 @@ import styles from './pokemon-list-ui-styles.js';
 import '@bbva-web-components/bbva-core-heading/bbva-core-heading.js';
 import '@bbva-web-components/bbva-button-default/bbva-button-default.js';
 import '@bbva-web-components/bbva-foundations-spinner/bbva-foundations-spinner.js';
-/**
- * ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
- *
- * This component ...
- *
- * Example:
- *
- * ```html
- *   <pokemon-list-ui></pokemon-list-ui>
- * ```
- */
+import '@pokedex/pokemon-list-dm/pokemon-list-dm.js';
 
 export class PokemonListUi extends LitElement {
   static get properties() {
     return {
-      /**
-       * Description for property
-       */
       pokemonList: {
         type: Array,
       },
-      page: {
-        type: String,
-      },
-      loading: {
+      isLoading: {
         type: Boolean,
       },
     };
@@ -37,16 +21,22 @@ export class PokemonListUi extends LitElement {
 
   constructor() {
     super();
-    this.loading = false;
+    this.isLoading = false;
     this.pokemonList = [];
-    this.page = 'https://pokeapi.co/api/v2/pokemon';
-    this.loadPokemons();
   }
-
-  firstUpdated() {
+  async firstUpdated() {
     this.modal = this.shadowRoot.querySelector('dialog');
     this.spinner = this.shadowRoot.querySelector('bbva-foundations-spinner');
-    console.log(this.spinner);
+    this.pokemonListDm = this.shadowRoot.querySelector('pokemon-list-dm');
+    this.pokemonList = await this.pokemonListDm.loadPokemons();
+    this.addEventListener(
+      'load-pokemon-request-started',
+      () => (this.isLoading = true),
+    );
+    this.addEventListener(
+      'load-pokemon-request-success',
+      () => (this.isLoading = false),
+    );
   }
 
   static get styles() {
@@ -59,13 +49,12 @@ export class PokemonListUi extends LitElement {
 
       <div class="content-container">
         <div class="list">${this._gridTpl}</div>
-        ${this.loading
-          ? html`<bbva-foundations-spinner
-              with-mask
-            ></bbva-foundations-spinner>`
-          : html`<bbva-button-default @click=${(e) => this.loadPokemons()}
-              >Load more pokémos</bbva-button-default
-            >`}
+        ${this.isLoading
+          ? html`<bbva-foundations-spinner with-mask>
+            </bbva-foundations-spinner>`
+          : html`<bbva-button-default @click=${(e) => this.loadMorePokemons()}>
+              Load more pokémos
+            </bbva-button-default>`}
       </div>
       <dialog class="modal">
         <div class="modal-container">
@@ -76,6 +65,9 @@ export class PokemonListUi extends LitElement {
           ></bbva-button-default>
         </div>
       </dialog>
+      <pokemon-list-dm
+        page="https://pokeapi.co/api/v2/pokemon"
+      ></pokemon-list-dm>
       <slot></slot>
     `;
   }
@@ -84,27 +76,18 @@ export class PokemonListUi extends LitElement {
     return html`<div class="list__grid">
       ${this.pokemonList.map(
         (pokemon) => html`
-          <div
-            class="card"
-            @click=${(e) =>
-              this.goToEvolutions({
-                id: pokemon.id,
-                name: pokemon.name,
-                img: pokemon.sprites.other.home.front_default,
-              })}
-          >
+          <div class="card" @click=${(e) => this.checkEvolution(pokemon)}>
             <img
               class="card__img"
-              src="${pokemon.sprites.other.home.front_default ||
-              pokemon.sprites.other['official-artwork'].front_default}"
+              src="${pokemon.sprites.other.home.front_default}"
               alt="${pokemon.name}"
               height="100"
               width="100"
             />
             <p class="card__small">no. ${pokemon.id}</p>
-            <bbva-core-heading level="5"
-              >${pokemon.name.toUpperCase()}</bbva-core-heading
-            >
+            <bbva-core-heading level="5">
+              ${pokemon.name.toUpperCase()}
+            </bbva-core-heading>
             <p class="card__tags">
               ${pokemon.types.map(
                 (type) =>
@@ -119,60 +102,16 @@ export class PokemonListUi extends LitElement {
     </div>`;
   }
 
-  async loadPokemons() {
-    try {
-      this.loading = true;
-
-      const res = await fetch(this.page);
-      const data = await res.json();
-      this.page = data.next;
-
-      for (const pokemon of data.results) {
-        try {
-          const pokemonRes = await fetch(pokemon.url);
-          const pokemonData = await pokemonRes.json();
-
-          this.pokemonList = [...this.pokemonList, pokemonData];
-        } catch {
-          console.error('ERROR', error);
-        }
-      }
-    } catch (error) {
-      console.error('ERROR', error);
-    } finally {
-      this.loading = false;
-    }
+  async loadMorePokemons() {
+    this.pokemonList = [
+      ...this.pokemonList,
+      ...(await this.pokemonListDm.loadPokemons()),
+    ];
   }
-
-  async goToEvolutions(pokemonInfo) {
-    const species = await this.fetchSpecies(pokemonInfo.name);
-    const evolution = await this.fetchEvolutionChain(
-      species.evolution_chain.url,
-    );
-    if (evolution.chain.evolves_to.length) {
-      console.log(pokemonInfo.name, 'Tiene evolución (^o^)/');
-      const selectedPokemon = {
-        ...pokemonInfo,
-        evolution,
-      };
-      console.log(selectedPokemon);
+  checkEvolution(pokemon) {
+    if (pokemon.evolution.chain.evolves_to.length) {
     } else {
       this.modal.showModal();
-      console.log(pokemonInfo.name, 'No tiene evolución (T｡T)');
     }
-  }
-
-  async fetchSpecies(name) {
-    const speciesRes = await fetch(
-      `https://pokeapi.co/api/v2/pokemon-species/${name}`,
-    );
-    const speciesData = await speciesRes.json();
-    return speciesData;
-  }
-
-  async fetchEvolutionChain(url) {
-    const evolutionChainRes = await fetch(url);
-    const evolutionChainData = await evolutionChainRes.json();
-    return evolutionChainData;
   }
 }
